07860dbdaac1397fe6b4997862f8d6a874d00b24
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-02-25 19:18:36 +0700

    update dot

--- metbit/__init__.py
+++ metbit/__init__.py
@@ -1,19 +1,19 @@
 # -*- coding: utf-8 -*-
 
 # utility
-from utility import unipair, gen_page
-from lingress import lin_regression
+from .utility import unipair, gen_page
+from .lingress import lin_regression
 
-from ellipse import Ellipse
+from .ellipse import Ellipse
 
-from opls import OPLS
+from .opls import OPLS
 
-from anova import anova_oplsda
+from .anova import anova_oplsda
 
-from vip import vip
+from .vip import vip
 
-import cross_validation
-import plotting
+import .cross_validation
+import .plotting
 
 # data preprocessing
 
--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
 setup(
   name = 'metbit',      
   packages = ['metbit'], 
-  version = '1.0.6',  
+  version = '1.0.7',  
   license='MIT', 
   description = 'Metabolomics data analysis and visualization tools.',
   long_description=DESCRIPTION,
   author = 'aeiwz',                 
   author_email = 'theerayut_aeiw_123@hotmail.com',     
   url = 'https://github.com/aeiwz/metbit.git',  
-  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V1.0.6.tar.gz',  
+  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V1.0.7.tar.gz',  
   keywords = ['Omics', 'Chemometrics', 'Visualization', 'Data Analysis'],
   install_requires=[            
           'scikit-learn',
