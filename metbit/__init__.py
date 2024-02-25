e1007b20d96899ff4d8b8d033c6c37bbe820946e
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-02-25 17:06:21 +0700

    update init

--- metbit/__init__.py
+++ metbit/__init__.py
@@ -1,27 +1,27 @@
 # -*- coding: utf-8 -*-
 
 # utility
-from metbit.utility.unipair import unipair
-from metbit.utility.genpage import genpage
+from utility.unipair import unipair
+from utility.genpage import genpage
 
 # preprocessing
-from metbit.preprocessing.denoise_spec import decrease_noise
-from metbit.preprocessing.normspec import Normalization
+from preprocessing.denoise_spec import decrease_noise
+from preprocessing.normspec import Normalization
 
 # multivariate
-from metbit.multivariate.opls.anova import *
-from metbit.multivariate.opls.base import *
-from metbit.multivariate.opls.boxplot import *
-from metbit.multivariate.opls.cross_validation import *
-from metbit.multivariate.opls.elipse import *
-from metbit.multivariate.opls.opls import *
-from metbit.multivariate.opls.plotting import *
-from metbit.multivariate.opls.pls import *
-from metbit.multivariate.opls.pretreatment import *
-from metbit.multivariate.opls.vip import *
+from multivariate.opls.anova import *
+from multivariate.opls.base import *
+from multivariate.opls.boxplot import *
+from multivariate.opls.cross_validation import *
+from multivariate.opls.elipse import *
+from multivariate.opls.opls import *
+from multivariate.opls.plotting import *
+from multivariate.opls.pls import *
+from multivariate.opls.pretreatment import *
+from multivariate.opls.vip import *
 
 # univariate
-from metbit.univariate.lingress import lingress
+from univariate.lingress import lingress
 
 
 # __all__ = ['unipair', 'genpage', 'decrease_noise', 'Normalization', 'anova', 'base', 'boxplot', 'cross_validation', 'elipse', 'opls', 'plotting', 'pls', 'pretreatment', 'vip', 'lingress']
