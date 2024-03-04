560ae28e4c2705dc051b86bcf54b184b6aaba70a
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-03-04 10:41:11 +0700

    update to v1.2.2

--- metbit/__init__.py
+++ metbit/__init__.py
@@ -1,3 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from .metbit import *
+from .metbit import opls_da, pca
+from .base import *
+from .boxplot import *
+from .cross_validation import *
+from .denoise_spec import *
+from .opls import *
+from .pca_ellipse import *
+from .plotting import *
+from .pls import *
+from .pretreatment import *
+from .spec_norm import *
+from .utility import gen_page, oplsda_path
+from .vip import *
