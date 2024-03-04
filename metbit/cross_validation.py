1b64a60a7d8ed549395793ecc75c3ec78af6668a
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-03-04 22:37:03 +0700

    Update cross_validation.py

--- metbit/cross_validation.py
+++ metbit/cross_validation.py
@@ -14,6 +14,17 @@ import numpy.linalg as la
 
 
 class CrossValidation:
+
+
+    from .pls import PLS
+    from .opls import OPLS
+    from .pretreatment import Scaler
+    import collections
+    import typingoo
+    import numpy as np
+    import numpy.linalg as la
+
+
     """
     Stratified cross validation
 
@@ -37,11 +48,17 @@ class CrossValidation:
     CrossValidation object
 
     """
+    
     def __init__(self, estimator="opls", kfold=10, scaler="pareto") -> None:
         
         from .pls import PLS
         from .opls import OPLS
         from .pretreatment import Scaler
+        import collections
+        import typing
+        import numpy as np
+        import numpy.linalg as la
+
         # number of folds
         self.kfold = kfold
         # estimator
