0532d4ac00a376d7ec0e1b0ac6e90dc7c208814b
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-06-27 10:17:28 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,14 +12,14 @@ DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
 setup(
   name = 'metbit',      
   packages = ['metbit'], 
-  version = '4.1.2',  
+  version = '4.1.3',  
   license='MIT', 
   description = 'Metabolomics data analysis and visualization tools.',
   author = 'aeiwz',                 
   author_email = 'theerayut_aeiw_123@hotmail.com',   
   url = 'https://github.com/aeiwz/metbit.git',  
-  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V4.1.2.tar.gz',  
-  keywords = ['Omics', 'Chemometrics', 'Visualization', 'Data Analysis'],
+  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V4.1.3.tar.gz',  
+  keywords = ['Omics', 'Multivariate analysis', 'Visualization', 'Data Analysis', 'Metabolomics', 'Chemometrics'],
   install_requires=[            
           'scikit-learn',
           'pandas',
