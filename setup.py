77e1e9b664f501830e6bb151aef5d0b791b0cbc3
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-07-03 14:45:33 +0700

    ellips in lazy

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
 setup(
   name = 'metbit',      
   packages = ['metbit'], 
-  version = '4.4.7',  
+  version = '4.4.8',  
   license='MIT', 
   description = 'Metabolomics data analysis and visualization tools.',
   author = 'aeiwz',                 
   author_email = 'theerayut_aeiw_123@hotmail.com',   
   url = 'https://github.com/aeiwz/metbit.git',  
-  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V4.4.7.tar.gz',  
+  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V4.4.8.tar.gz',  
   keywords = ['Omics', 'Multivariate analysis', 'Visualization', 'Data Analysis', 'Metabolomics', 'Chemometrics'],
   install_requires=[            
           'scikit-learn',
