44cc23ba24cd837a25d21053649ceac2ef81c903
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-10-10 11:26:31 +0700

    add peak chop and nomalise

--- metbit/__init__.py
+++ metbit/__init__.py
@@ -3,6 +3,8 @@
 from .metbit import opls_da, pca
 from .utility import *
 from .lazy_opls_da import lazy_opls_da
+from .spec_norm import *
+from .peak_processe import peak_chops
 
 __author__ = 'aeiwz'
 __email__ = 'theerayut_aeiw_123@hotmail.com'
