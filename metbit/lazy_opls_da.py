33e751faed63ebda104c0f9ea675efa72fa72aae
CAS9 <theerayut_aeiw_123@hotmail.com>
2024-11-13 16:15:11 +0700

    fix bug and add document

--- metbit/lazy_opls_da.py
+++ metbit/lazy_opls_da.py
@@ -45,6 +45,7 @@ class lazy_opls_da:
         n_components: int
             The number of components to use.
         lazy_opls_da(data, y, n_components).fit()
+
         """
 
 
@@ -90,6 +91,7 @@ class lazy_opls_da:
         n_components: int
             The number of components to use.
         lazy_opls_da(data, y, n_components).fit()
+
         """
         
         project_name = project_name_generator()
@@ -179,6 +181,45 @@ class lazy_opls_da:
 
     def fit(self, marker_color: dict = None, custom_color: list = None, custom_shape: list = None, symbol_dict: dict = None, custom_legend_name = ['Group', 'Sub-group'], marker_label=None, marker_size=None, marker_opacity=None, individual_ellipse=False) -> None:
 
+
+        """
+        Fit the OPLS-DA model with custom visualization settings and generate various plots and data exports.
+
+        Parameters:
+        - marker_color (dict): A dictionary mapping unique group labels to colors for plotting. Defaults to a palette if not provided.
+        - custom_color (list): List to override group labels for color categorization in the data.
+        - custom_shape (list): List to override subgroup labels for shape categorization in the data.
+        - symbol_dict (dict): Dictionary mapping unique group labels to specific symbols for plotting. A random symbol is assigned if None.
+        - custom_legend_name (list): List of names for legend categories. Default is ['Group', 'Sub-group'].
+        - marker_label: Label to apply to markers in the plots (index, group, sub-group, class).
+        - marker_size: Size of markers in plots. If None, defaults are set based on data size.
+        - marker_opacity: Opacity of markers in plots. Default is 0.7 if None.
+        - individual_ellipse (bool): If True, individual ellipses are plotted per group in the score plot.
+
+        Workflow:
+        1. **Initialize Parameters**: Set color and shape settings based on provided parameters or default values.
+        2. **Assign Symbols**: Assign symbols to unique groups if not specified.
+        3. **Data Preparation**: Split data by 'Class' and initialize metadata for each subset.
+        4. **OPLS-DA Model Fitting**:
+        - Fit the OPLS-DA model for each data subset with specified components, scaling, and other model parameters.
+        - Save score plots (HTML and PNG), scores data (CSV), loading plots, and S-plots.
+        5. **Optional VIP Scores**: Calculate and save VIP scores and plots if enabled.
+        6. **Permutation Test**: Perform permutation tests if enabled and save histograms and scores.
+        7. **Linear Regression**: Fit a linear regression model if enabled and generate volcano plots and reports.
+
+        Outputs:
+        - Various plot files (.html, .png) saved in designated paths within `path` dictionary attribute.
+        - Data exports (.csv) for scores, loading plots, VIP scores, permutation test results, and regression data.
+
+        Returns:
+        - A success message confirming the model fitting completion.
+
+        Example:
+        ```python
+        model.fit(marker_color={'Group1': 'blue', 'Group2': 'green'}, custom_shape=['circle', 'square'])
+        ```
+        """
+
         from .metbit import opls_da
         from lingress import lin_regression
         from .utility import lazypair
@@ -266,6 +307,20 @@ class lazy_opls_da:
             else: 
                 feature_names = feature_names
             y = meta['Class']
+
+
+            if marker_label is not None:
+                marker_label = marker_label
+            
+            else:
+                if marker_label == 'class':
+                    marker_label = meta['class']
+                elif marker_label == 'group':
+                    marker_label = meta['Group']
+                elif marker_label == 'sub-group':
+                    marker_label = meta['Sub-group']
+                else:
+                    marker_label = meta.index
 
 
             #OPLS-DA
