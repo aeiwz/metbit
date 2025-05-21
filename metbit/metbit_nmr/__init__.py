2392d5bbc5cdf9df740e815928df9645c3f73479
CAS9 <theerayut_aeiw_123@hotmail.com>
2025-05-21 10:07:27 +0700

    update patch

---
+++ metbit/metbit_nmr/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .nmr_preprocess import *
+from .calibrate import *
+
+# -*- coding: utf-8 -*-
+
+__author__ = "aeiwz"
+__email__ = "theerayut_aeiw_123@hotmail.com"
+__maintainer__ = "aeiwz"
+__status__ = "Development"
+__copyright__ = "Copyright 2024, Theerayut"
--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
 setup(
   name = 'metbit',      
   packages = ['metbit'], 
-  version = '7.3.11',  
+  version = '7.3.12',  
   license='MIT', 
   description = 'Metabolomics data analysis and visualization tools.',
   author = 'aeiwz',                 
