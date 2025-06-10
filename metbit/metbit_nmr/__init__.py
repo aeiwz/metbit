f2bd06c78c6779a0ebeed03d160cd320b8cef167
aeiwz <theerayut_aeiw_123@hotmail.com>
2025-06-10 09:54:12 +0700

    Delete __init__.py

--- metbit/metbit_nmr/__init__.py
+++
@@ -1,9 +0,0 @@
-
-
-# -*- coding: utf-8 -*-
-
-__author__ = "aeiwz"
-__email__ = "theerayut_aeiw_123@hotmail.com"
-__maintainer__ = "aeiwz"
-__status__ = "Development"
-__copyright__ = "Copyright 2024, Theerayut"
