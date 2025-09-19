7c8bb904fc11d71f86134283d64ff7e50f7042e8
aeiwz <77935765+aeiwz@users.noreply.github.com>
2025-09-19 16:48:58 +0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ DESCRIPTION = '\n\n'.join(LOAD_TEXT(_) for _ in [
 setup(
   name = 'metbit',
   packages = ['metbit'],
-  version = '8.7.0',
+  version = '8.7.1',
   license='MIT',
   description = 'Metabolomics data analysis and visualization tools.',
   author = 'aeiwz',
   author_email = 'theerayut_aeiw_123@hotmail.com',
   url = 'https://github.com/aeiwz/metbit.git',
-  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V8.7.0.tar.gz',
+  download_url = 'https://github.com/aeiwz/metbit/archive/refs/tags/V8.7.1.tar.gz',
   keywords = ['Omics', 'Multivariate analysis', 'Visualization', 'Data Analysis', 'Metabolomics', 'Chemometrics'],
   install_requires=[
           'scikit-learn',
